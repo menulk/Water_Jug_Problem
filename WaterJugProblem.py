c4247fd105f356ea5c748287cbc00189e87019c2
M. Lakshika <39967581+menulk@users.noreply.github.com>
2021-05-23 17:55:03 +0530

    Update WaterJugProblem.py
    Search and possible operators

--- WaterJugProblem.py
+++ WaterJugProblem.py
@@ -37,9 +37,126 @@ def get_target(jugs):
         
     return target_capacity
 
-##
+##########
+#Check the jugs reached to Target capacity
+def is_target(path, target_capacity):
+    return path[-1][0] == target_capacity or path[-1][1] == target_capacity
 
-#**
+def been_there(node, check_dict):
+    print(" {0} is a before visited state ".format(node))
+    return check_dict.get(get_index(node), False)
+
+def next_transitions(jugs, path, check_dict):
+   
+    print("Search next state/ position")
+    result = []
+    next_nodes = []
+    node = []
+    
+    a_max = jugs[0]
+    b_max = jugs[1]
+    
+    # Start state Jug 01
+    a = path[-1][0]  
+    # Start state Jug 02
+    b = path[-1][1]  
+
+    # 1. Fill 4l/ First jug
+    node.append(a_max)
+    node.append(b)
+    if not been_there(node, check_dict):
+        next_nodes.append(node)
+    node = []
+
+    # 2. Fill 3l/ Second jug
+    node.append(a)
+    node.append(b_max)
+    if not been_there(node, check_dict):
+        next_nodes.append(node)
+    node = []
+
+    # 3. 3l to 4l / 2nd jug to 1st jug
+    node.append(min(a_max, a + b))
+    node.append(b - (node[0] - a))  # b - ( a' - a)
+    if not been_there(node, check_dict):
+        next_nodes.append(node)
+    node = []
+
+    # 4. 4l to 3l / 1st jug to 2nd jug
+    node.append(min(a + b, b_max))
+    node.insert(0, a - (node[0] - b))
+    if not been_there(node, check_dict):
+        next_nodes.append(node)
+    node = []
+
+    # 5. Empty 4l/ 1st jug
+    node.append(0)
+    node.append(b)
+    if not been_there(node, check_dict):
+        next_nodes.append(node)
+    node = []
+
+    # 6. Empty 3l/ 2nd jug
+    node.append(a)
+    node.append(0)
+    if not been_there(node, check_dict):
+        next_nodes.append(node)
+
+
+    # Neighbour paths
+    for i in range(0, len(next_nodes)):
+        temp = list(path)
+        temp.append(next_nodes[i])
+        result.append(temp)
+
+    if len(next_nodes) == 0:
+        print("No unvisited nodes\n\nTurn back")
+    else:
+        print("Possible states: ")
+        for nnode in next_nodes:
+            print(nnode)
+
+    return result
+
+
+def transition(old, new, jugs):
+    
+    a = old[0]
+    b = old[1]
+    a_prime = new[0]
+    b_prime = new[1]
+    a_max = jugs[0]
+    b_max = jugs[1]
+
+    if a > a_prime:
+        if b == b_prime:
+            return "Clear {0}-liter jug:\t\t".format(a_max)
+        else:
+            return "Pour {0}-liter jug into {1}-liter jug:\t".format(a_max, b_max)
+    else:
+        if b > b_prime:
+            if a == a_prime:
+                return "Clear {0}-liter jug:\t\t".format(b_max)
+            else:
+                return "Pour {0}-liter jug into {1}-liter jug:\t".format(b_max, a_max)
+        else:
+            if a == a_prime:
+                return "Fill {0}-liter jug:\t\t".format(b_max)
+            else:
+                return "Fill {0}-liter jug:\t\t".format(a_max)
+
+
+def print_path(path, jugs):
+  
+    print("Start State :", path[0])
+    for i in  range(0, len(path) - 1):
+        print(i+1,":", transition(path[i], path[i+1], jugs), path[i+1])
+
+
+
+
+##########
+
 def search(start_node, jugs, target_capacity, check_dict):
     
     target = []
